a4043b640886ff67f635db2ff104603a1ef8678a
maki toda <earth.in.us@gmail.com>
2017-03-31 10:39:36 -0700

    modified AppDelegate.swift.

--- FirebaseSocialLogin/FirebaseSocialLogin/AppDelegate.swift
+++ FirebaseSocialLogin/FirebaseSocialLogin/AppDelegate.swift
@@ -5,16 +5,25 @@
 
 import UIKit
 import CoreData
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
         return true
+    }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        
+        let handled = FBSDKApplicationDelegate.sharedInstance() .application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+        
+        return handled
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -87,4 +96,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 }
-
