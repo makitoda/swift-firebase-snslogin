2ea73fb1c5181b7e1a7df4d7b41f0b7693439337
maki toda <earth.in.us@gmail.com>
2017-03-31 11:28:23 -0700

    Show a facebook login button.

--- FirebaseSocialLogin/FirebaseSocialLogin/ViewController.swift
+++ FirebaseSocialLogin/FirebaseSocialLogin/ViewController.swift
@@ -4,19 +4,68 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let loginButton = FBSDKLoginButton()
+        view.addSubview(loginButton)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addConstraints([
+            NSLayoutConstraint(
+                item: loginButton,
+                attribute: NSLayoutAttribute.top,
+                relatedBy: NSLayoutRelation.equal,
+                toItem: self.view,
+                attribute: NSLayoutAttribute.top,
+                multiplier: 1.0,
+                constant: 50
+            ),
+            NSLayoutConstraint(
+                item: loginButton,
+                attribute: NSLayoutAttribute.left,
+                relatedBy: NSLayoutRelation.equal,
+                toItem: self.view,
+                attribute: NSLayoutAttribute.left,
+                multiplier: 1.0,
+                constant: 16
+            ),
+            NSLayoutConstraint(
+                item: loginButton,
+                attribute: NSLayoutAttribute.width,
+                relatedBy: NSLayoutRelation.equal,
+                toItem: self.view,
+                attribute: NSLayoutAttribute.width,
+                multiplier: 1.0,
+                constant: -32
+            ),
+            NSLayoutConstraint(
+                item: loginButton,
+                attribute: NSLayoutAttribute.height,
+                relatedBy: NSLayoutRelation.equal,
+                toItem: nil,
+                attribute: NSLayoutAttribute.height,
+                multiplier: 1.0,
+                constant: 60
+            )])
+        
+        loginButton.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Did log out of facebook")
     }
-
-
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print(error)
+            return
+        }
+        
+        print("Successfully logged in with facebook...")
+    }
 }
 
